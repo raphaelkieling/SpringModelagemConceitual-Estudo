357b2f5434212911f63ba3a57fd0bab655fc8f40
Raphael Kieling <rapha_kieling@hotmail.com>
2018-08-23 22:40:30 -0300

    Testando primeira classe de domínio

---
+++ src/main/java/com/raphaelkieling/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.raphaelkieling.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raphaelkieling.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List listar() {
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+	
+}
